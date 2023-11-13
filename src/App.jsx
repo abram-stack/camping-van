cd6e1d73d170039afba44415bca3f56713d3bb5c
abram-stack <anthonybramantyo@outlook.com>
2023-11-13 11:23:44 +0100

    git init, setup header, home, about-page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About';
+
+function App() {
+  return(
+    <BrowserRouter>
+      <header>
+        <Link className='site-logo' to='/'>#Vanlife</Link>
+        <nav>
+          <Link to='/about'>about</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
